af513b58c892d7b2abaff7b449fc4bd7be3103fe
Sebastian Rodriguez <ply.srh@gmail.com>
2025-12-17 23:18:58 -0300

    chore: updating the oauth function

--- demos/typescript/api/oauth.ts
+++ demos/typescript/api/oauth.ts
@@ -9,8 +9,8 @@ export default async function handler(request: VercelRequest, response: VercelRe
 
   const { provider, code } = request.body;
 
-  console.log('FUNCTION_REQUEST', JSON.stringify(request));
   console.log('FUNCTION_ENV_VARS', ENV_VARS);
+  console.log('FUNCTION_REQUEST', request.body, request.query, request.cookies);
 
   if (!provider) {
     return response.status(400).json({ error: 'Missing provider' });
